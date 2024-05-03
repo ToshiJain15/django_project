3d4efd90674e59191e79a2fac1aab25c98aa6bf8
ToshiJain15 <jaintoshi15@gmail.com>
2024-05-03 09:49:40 +0530

    models

--- vendor/performance/models.py
+++ vendor/performance/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class HistoricalPerformance(models.Model):
+  vendor = models.ForeignKey()
+  date = models.DateTimeField()
+  on_time_delivery_rate = models.FloatField()
+  quality_rating_avg = models.FloatField()
+  average_response_time = models.FloatField()
+  fulfillment_rate = models.FloatField()
--- vendor/purchase_order/models.py
+++ vendor/purchase_order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class PurchaseOrder(models.Model):
+  po_number = models.CharField(max_length=255)
+  vendor = models.ForeignKey()
+  order_date = models.DateTimeField()
+  delivery_date = models.DateTimeField()
+  items = models.JSONField()
+  quantity = models.IntegerField()
+  status = models.CharField(max_length=255)
+  quality_rating = models.FloatField()
+  issue_date = models.DateTimeField()
+  acknowledgement_date = models.DateTimeField()
